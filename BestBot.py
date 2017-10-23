228b82d947a4423542d190a57203c24eff853f54
Tyler <tmacinnisj@gmail.com>
2017-10-23 18:14:16 -0400

    Just a bunch of comments.  Getting ideas down as well as a general format for the commands and functions

--- BestBot.py
+++ BestBot.py
@@ -11,6 +11,8 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:˓→%(message)s'))
 logger.addHandler(handler)
 
+
+#TODO: reformat permissions, Figure this out
 with open('config.json', 'r') as f:
     config = json.load(f)
 
@@ -23,6 +25,9 @@ async def on_ready():
     print(bot.user.id)
     print('-----------')
 
+#Bot reactions, Personality
+
+#Good bot response
 @bot.command(pass_context = True)
 async def ot(ctx):
     msg = []
@@ -31,17 +36,76 @@ async def ot(ctx):
     previous_message_author = msg[-1].author
     if "BestBot" in previous_message_author.name and previous_message_author.bot:
         await bot.send_message(ctx.message.channel, 'Thank you!')
-    else:
-        if "BestBot" not in previous_message_author.name and previous_message_author.bot:
+    #else:
+        #if "BestBot" not in previous_message_author.name and previous_message_author.bot:
             #TODO: Wait for response from other bot
             #asyncio.sleep(1)
             #msg = []
             #async for x in bot.logs_from(ctx.message.channel, limit=2):
             #    msg.append(x)
             #if "Thank" not in msg[-1].content or "thank" not in msg[-1].content:
-            await bot.send_message(ctx.message.channel, '@{0.name} Say thank you!'.format(previous_message_author))
+            #await bot.send_message(ctx.message.channel, '@{0.name} Say thank you!'.format(previous_message_author))
+
+#Bad bot response?
+#Responding to random phrases
+#Maybe respond to nicknames?
 
 
+
+#Recipe
+        #TODO: Create Recipe JSON
+
+#Remove Recipe
+    # Check permissions
+    #Validate (!RemoveRecipe Name)
+    #Check if name exists
+    #Update JSON
+
+#Add Recipe
+    #Check permissions
+    #Validate input (!AddRecipe Name Type URL)
+        #Make Sure URL is valid
+    #?Maybe add who added the recipe?
+    #Update JSON
+
+#Recall recipe
+    #Check permissions
+    #Validate input (!Recipe OptionalType) (!Recipe Name) (!Recipe)
+    #If type Recall random recipe of type
+    #Else Recall random recipe
+
+
+
+#Reaction GIF
+        #TODO: Create Reaction gif JSON
+
+#Add Reaction
+    #Check permissions
+    #Validate input(!AddReaction Name URL OptionalMood)
+    #Validate URL
+    #Make sure name isn't already taken by name or mood
+    #Update JSON
+    #Let user know it's been added (By whisper? or post?)
+
+#Remove Reaction
+    #Check permissions
+    #Validate Input(!RemoveReaction Name)
+    #Check if name exists
+    #Update JSON
+
+#Recall Reaction
+    #Check permissions
+    #Validate Input (!!Name or !!Mood)
+    #Check if mood exists
+        #Post Random gif from mood
+    #Else check if name exists
+        #Post Reaction
+
+
+
+#Utility
+
+#Shutdown
 @bot.command(pass_context = True)
 async def ls(ctx):
     msg = []
@@ -53,5 +117,21 @@ async def ls(ctx):
         raise SystemExit
     else:
         await bot.send_message(ctx.message.channel, 'You\'re not an admin!')
+
+#Update Permissions
+
+#Check Permissions
+    #Whisper @user, Maybe check for specific functions?
+
+#Uptime
+
+#Figure out a restart function
+
+#Function for Contributor/Admin check
+    #Make sure someone is actually allowed to use bot functions
+    #Selective (Each bot function should have a permissions value? Something equivalent
+        # Admin Contributor User Banned)?
+
+
 
 bot.run(config["bot_token"])
